744a82e9b686bd1b92fc2056cc6e26fb09bbb5d8
liuyunan <liuyunan.20@gmail.com>
2022-02-01 12:31:12 -0800

    test said it may have an infinite loop

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -13,12 +13,13 @@ class Translator:
         self.source = None
         self.target = None
         self.word = None
+        self.translations = []
+        self.sentences = []
 
     def choose_language(self, source, target, word):
         self.source = self.languages[source]
         self.target = self.languages[target]
         self.word = word
-        print(f'You chose "{self.target}" as a language to translate "{self.word}".')
         return f"https://context.reverso.net/translation/{self.source}-{self.target}/{self.word}"
 
     def get_translations(self, source, target, word):
@@ -28,23 +29,67 @@ class Translator:
             page = requests.get(url, headers=headers)
             print(url)
             if page.status_code == 200:
-                print("200 OK")
-
                 soup = BeautifulSoup(page.content, "html.parser")
-                translation_links = soup.find_all("a", {"class": "ltr"})
-                print(f"{self.target} Translations:")
-
+                # get word translations, save in a list
+                translation_links = soup.find_all("a", {"class": "translation"})
                 for link in translation_links:
-                    print(link.text.replace(" ", "").replace("\n", "").replace("\r", ""))
-                print()
+                    self.translations.append(link.text.replace(" ", "").replace("\n", "").replace("\r", ""))
+                # get example sentences, save in a list
                 examples = soup.find_all("div", {"class": "example"})
-                print(f"{self.target} Examples:")
                 for example in examples:
+                    exp_sentence = example.find_all("span", {"class": "text"})
+                    sentence = []
+                    for exp in exp_sentence:
+                        sentence.append(exp.text.strip().replace("\n", "").replace("\r", ""))
+                    self.sentences += sentence
+                break
+
+    def save_translations(self):
+        with open(f"{self.word}.txt", "a") as tran_file:
+            tran_file.writelines(self.translations)
+            tran_file.writelines(self.sentences)
+
+    def print_translations(self):
+        print(f"{self.target} Translations:")
+        for translation in self.translations:
+            if translation != "Translation":
+                print(translation)
+        print()
+        print(f"{self.target} Examples:")
+        for sentence in self.sentences:
+            print(sentence)
+        print()
+
+    def translate_all(self, source, word):
+        for x in self.languages:
+            if x != source:
+                # self.get_translations(source, x, word)
+                # print(f"{self.target} Translations:")
+                # print(self.translations[1])
+                # print()
+                # print(f"{self.target} Example:")
+                # print(self.sentences[0])
+                # print(self.sentences[1])
+                # print()
+                url = self.choose_language(source, x, word)
+                headers = {'User-Agent': 'Mozilla/5.0'}
+                page = requests.get(url, headers=headers)
+                print(url)
+                if page.status_code == 200:
+                    soup = BeautifulSoup(page.content, "html.parser")
+                    translation_links = soup.find_all("a", {"class": "translation"})
+                    for link in translation_links:
+                        self.translations.append(link.text.replace(" ", "").replace("\n", "").replace("\r", ""))
+                    print(f"{self.target} Translations:")
+                    print(self.translations[1])
+                    print()
+                    example = soup.find("div", {"class": "example"})
+                    print(f"{self.target} Example:")
                     exp_sentence = example.find_all("span", {"class": "text"})
                     for exp in exp_sentence:
                         print(exp.text.strip().replace("\n", "").replace("\r", ""))
+                        self.sentences.append(exp.text.strip().replace("\n", "").replace("\r", ""))
                     print()
-                break
 
 
 print('''Hello, you're welcome to the translator. Translator supports: 
@@ -63,9 +108,14 @@ print('''Hello, you're welcome to the translator. Translator supports:
 13. Turkish
 Type the number of your language: ''')
 user_source = input()
-print("Type the number of language you want to translate to: ")
+print("Type the number of language you want to translate to or '0' to translate to all languages: ")
 user_target = input()
 print('Type the word you want to translate:')
 user_word = input()
 translator = Translator()
-translator.get_translations(user_source, user_target, user_word)
+if user_target == "0":
+    translator.translate_all(user_source, user_word)
+else:
+    translator.get_translations(user_source, user_target, user_word)
+    translator.print_translations()
+translator.save_translations()
