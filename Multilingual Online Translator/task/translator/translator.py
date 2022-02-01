2e4d2803f422f7da131bace1ef7e905653ee0a36
liuyunan <liuyunan.20@gmail.com>
2022-02-01 00:23:26 -0800

    print translation in particular format

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -2,38 +2,50 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def choose_language():
-    trans_pair = {"en": ["french", "english"], "fr": ["english", "french"]}
-    print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
-    ipt = input()
-    source = trans_pair[ipt][0]
-    target = trans_pair[ipt][1]
-    print('Type the word you want to translate:')
-    word = input()
-    print(f'You chose "{target}" as a language to translate "{word}".')
-    return f"https://context.reverso.net/translation/{source}-{target}/{word}"
+class Translator:
+    def __init__(self, ipt, word):
+        self.ipt = ipt
+        self.source = None
+        self.target = None
+        self.word = word
+
+    def choose_language(self):
+        trans_pair = {"en": ["french", "english"], "fr": ["english", "french"]}
+
+        self.source = trans_pair[self.ipt][0]
+        self.target = trans_pair[self.ipt][1]
+
+        return f"https://context.reverso.net/translation/{self.source}-{self.target}/{self.word}"
+
+    def get_translations(self):
+        print(f'You chose "{self.target}" as a language to translate "{self.word}".')
+        url = self.choose_language()
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        while True:
+            page = requests.get(url, headers=headers)
+            if page.status_code == 200:
+                print("200 OK")
+
+                soup = BeautifulSoup(page.content, "html.parser")
+                translation_links = soup.find_all("a", {"class": "translation"})
+                print(f"{self.target} Translations:")
+                for link in translation_links:
+                    print(link.text.replace(" ", "").replace("\n", "").replace("\r", ""))
+                print()
+                examples = soup.find_all("div", {"class": "example"})
+                print(f"{self.target} Examples:")
+                for example in examples:
+                    exp_sentence = example.find_all("span", {"class": "text"})
+                    for exp in exp_sentence:
+                        print(exp.text.strip().replace("\n", "").replace("\r", ""))
+                    print()
+                break
 
 
-url = choose_language()
-headers = {'User-Agent': 'Mozilla/5.0'}
-while True:
-    page = requests.get(url, headers=headers)
-    if page.status_code == 200:
-        print("200 OK")
-        print("Translations")
-        soup = BeautifulSoup(page.content, "html.parser")
-        translation_links = soup.find_all("a", {"class": "translation"})
-        translations = []
-        for link in translation_links:
-            translations.append(link.text.replace(" ", "").replace("\n", "").replace("\r", ""))
-        examples = soup.find_all("div", {"class": "example"})
-        sentences = []
-        for example in examples:
-            exp_sentence = example.find_all("span", {"class": "text"})
-            sentence = []
-            for exp in exp_sentence:
-                sentence.append(exp.text.strip().replace("\n", "").replace("\r", ""))
-            sentences += sentence
-        break
-print(translations)
-print(sentences)
+print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
+user_target = input()
+print('Type the word you want to translate:')
+user_word = input()
+translator = Translator(user_target, user_word)
+translator.choose_language()
+translator.get_translations()
